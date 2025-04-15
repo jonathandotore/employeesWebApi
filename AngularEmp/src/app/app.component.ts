c9cb7a59e85386a3cfab8593a04ad39117986f9c
Jonathan Dotore <jonathandotore@outlook.com>
2025-04-15 00:18:16 -0300

    [FIX] Imported router module to do redirects

--- AngularEmp/src/app/app.component.ts
+++ AngularEmp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { FormsModule } from '@angular/forms'
 import { ReactiveFormsModule } from '@angular/forms';
 
--- AngularEmp/src/app/app.routes.ts
+++ AngularEmp/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EditComponent } from './pages/edit/edit.component';
 
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
     { path: '', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'edit/:id', component: EditComponent },
 ];
